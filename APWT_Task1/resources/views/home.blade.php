446df3cc1af78017b037b302c927dbae037941a8
Rabby3075 <rashed.rabby43@gmail.com>
2022-06-01 22:19:13 +0600

    task1 homepage done

---
+++ APWT_Task1/resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layout.home')
+@section('content')
+<style>
+    .centering h1{
+
+        color: red;
+    }
+</style>
+<div id="container">
+<h1 class="text-primary text-center">Welcome</h1>
+</div>
+@endsection
---
+++ APWT_Task1/resources/views/navbar/navbar.blade.php
@@ -0,0 +1,26 @@
+<nav class="navbar navbar-expand-sm navbar-light bg-light">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="{{route('home')}}}">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Product/Service</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Our Teams</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About Us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact Us</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
